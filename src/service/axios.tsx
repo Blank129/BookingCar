66a35145b92eed1b80aff04851d52bbe7355edec
unknown <dungq2003@gmail.com>
2025-07-22 15:32:04 +0700

    config axios

--- src/service/axios.tsx
+++ src/service/axios.tsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 export const axiosApiUserInstance = axios.create({
-  baseURL: "http://localhost:5000/api/user",
+  baseURL: "https://booking-car-be.vercel.app/api/user",
 });
 
 export const axiosApiDriverInstance = axios.create({
-  baseURL: "http://localhost:5000/api/driver",
+  baseURL: "https://booking-car-be.vercel.app/api/driver",
 });
+
+//http://localhost:5000
+//https://booking-car-be.vercel.app
